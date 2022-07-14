a411f609813abcf5aa7a5430bddd4ced1c4d1daf
shengyu <shengyu@ShengYuPC>
2022-07-15 01:30:30 +0800

    init repositories

---
+++ regress/regression.py
@@ -0,0 +1,66 @@
+import sys
+import pandas as pd
+import numpy as np
+from google.colab import drive 
+
+data = pd.read_csv('./train.csv', encoding = 'big5')
+data = data.iloc[:, 3:]
+data[data == 'NR'] = 0
+raw_data = data.to_numpy()
+month_data = {}
+for month in range(12):
+    sample = np.empty([18, 480])
+    for day in range(20):
+        sample[:, day * 24 : (day + 1) * 24] = raw_data[18 * (20 * month + day) : 18 * (20 * month + day + 1), :]
+    month_data[month] = sample
+
+x = np.empty([12 * 471, 18 * 9], dtype = float)
+y = np.empty([12 * 471, 1], dtype = float)
+for month in range(12):
+    for day in range(20):
+        for hour in range(24):
+            if day == 19 and hour > 14:
+                continue
+            x[month * 471 + day * 24 + hour, :] = month_data[month][:,day * 24 + hour : day * 24 + hour + 9].reshape(1, -1) 
+            #vector dim:18*9 (9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9) reshape:变成一行
+            y[month * 471 + day * 24 + hour, 0] = month_data[month][9, day * 24 + hour + 9] #value
+print(x)
+print(y)
+#normalize
+mean_x = np.mean(x, axis = 0) #18 * 9 
+std_x = np.std(x, axis = 0) #18 * 9 
+for i in range(len(x)): #12 * 471
+    for j in range(len(x[0])): #18 * 9 
+        if std_x[j] != 0:
+            x[i][j] = (x[i][j] - mean_x[j]) / std_x[j]
+
+import math
+x_train_set = x[: math.floor(len(x) * 0.8), :]
+y_train_set = y[: math.floor(len(y) * 0.8), :]
+x_validation = x[math.floor(len(x) * 0.8): , :]
+y_validation = y[math.floor(len(y) * 0.8): , :]
+# print(x_train_set)
+# print(y_train_set)
+# print(x_validation)
+# print(y_validation)
+# print(len(x_train_set))
+# print(len(y_train_set))
+# print(len(x_validation))
+# print(len(y_validation))
+
+dim = 18 * 9 + 1 #加了常数项b
+w = np.zeros([dim, 1])
+x = np.concatenate((np.ones([12 * 471, 1]), x), axis = 1).astype(float)
+learning_rate = 100
+iter_time = 1000
+adagrad = np.zeros([dim, 1])
+eps = 0.0000000001
+for t in range(iter_time):
+    loss = np.sqrt(np.sum(np.power(np.dot(x, w) - y, 2))/471/12)#rmse
+    if(t%100==0):
+        print(str(t) + ":" + str(loss))
+    gradient = 2 * np.dot(x.transpose(), np.dot(x, w) - y) #dim*1
+    adagrad += gradient ** 2
+    w = w - learning_rate * gradient / np.sqrt(adagrad + eps)
+np.save('weight.npy', w)
+print(w)
